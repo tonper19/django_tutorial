0044542fe6b81b6f39b32c51593dbec7dcbd410b
Tony Perez <tonper19@gmail.com>
2020-06-15 19:52:32 +0200

    07. Writing your first Django app, part 7

--- tutorial/mysite/polls/tests.py
+++ tutorial/mysite/polls/tests.py
@@ -11,9 +11,9 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
+        is older than 60 days.
         """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        time = timezone.now() - datetime.timedelta(days=60, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
